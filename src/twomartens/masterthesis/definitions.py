05ea13d2525ccd2fb028d3b514ac5a09adf2ae50
Jim Martens <github@2martens.de>
2019-04-01 12:54:58 +0200

    Moved COCO wrapper into package
    Signed-off-by: Jim Martens <github@2martens.de>

---
+++ src/twomartens/masterthesis/definitions.py
@@ -0,0 +1,52 @@
+#  -*- coding: utf-8 -*-
+#
+#  Copyright 2019 Jim Martens
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""
+Provides defined constants.
+
+Attributes:
+    COCO_TO_WNID: specifies a mapping from COCO ids to WordNet ids
+"""
+
+COCO_TO_WNID = {
+    1: [],
+    2: ['04466871'],
+    25: ['02773838'],
+    27: ['02773838'],
+    40: ['03603722', '04591713', '02960903', '04591713', '02876657'],
+    42: ['03797390', '03063599'],
+    46: ['02880940'],
+    57: ['03001627', '03002711', '20000018', '20000024', '20000016', '20000015', '20000025', '04593077', '04062428',
+         '03168217', '20000023', '20000026', '20000022', '03786621', '08647616',
+         '03262932', '03649674', '04099969', '20000027', '20000020', '03376595', '02738535', '04373704', '04331277',
+         '04590933'],
+    58: ['03002711', '20000030', '20000028', '20000029', '04256520'],
+    59: ['03990474', '03956922', '03991062', '03957315', ],
+    60: ['20000007', '20000004', '02920259', '03165096', '03962852', '20000006'],
+    61: ['04398951', '03246933', '20000040', '04061681', '20000038', '03904060', '03092883', '20000039', '03063968',
+         '20000037', '20000041', '03620967', '03179701', '04379243'],
+    62: ['04446276'],
+    63: ['03782190', '03085219', '06277280'],
+    64: ['03782190', '03085219', '03642806'],
+    67: ['03085013'],
+    68: ['03261776', '03179910', '02992529', '04401088'],
+    70: ['04330267'],
+    72: ['04553920', '03325088', '03775636', '04559451', '04223580'],
+    74: ['06410904'],
+    75: ['03046257'],
+    76: ['03603722', '04522168', '02960903'],
+    80: ['04453156'],
+}
