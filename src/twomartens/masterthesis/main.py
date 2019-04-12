1ea9ff98d4bc1d64e7e5c7cdc5b38885ee3afc2d
Jim Martens <github@2martens.de>
2019-04-12 14:17:17 +0200

    Increased zsize to 64
    Signed-off-by: Jim Martens <github@2martens.de>

--- src/twomartens/masterthesis/main.py
+++ src/twomartens/masterthesis/main.py
@@ -111,7 +111,7 @@ def _auto_encoder_train(args: argparse.Namespace) -> None:
     with train_summary_writer.as_default():
         train.train_simple(coco_data, iteration=args.iteration,
                            weights_prefix=f"{args.weights_path}/category-{category}",
-                           zsize=8, lr=0.0001,
+                           zsize=64, lr=0.0001,
                            channels=3, train_epoch=args.num_epochs, batch_size=batch_size)
 
 
