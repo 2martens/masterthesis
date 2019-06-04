d3706042794d4d040613483a48bc441f424ceb28
Jim Martens <github@2martens.de>
2019-06-04 10:59:06 +0200

    Added another debug print
    Signed-off-by: Jim Martens <github@2martens.de>

--- src/twomartens/masterthesis/data.py
+++ src/twomartens/masterthesis/data.py
@@ -416,6 +416,7 @@ def prepare_scenenet_val(data_path: str, protobuf_path: str) -> Tuple[List[List[
                 if coordinates is None:  # the current instance was not in this frame
                     continue
                 else:
+                    print(instance_local)
                     print(coordinates)
                     coordinates = coordinates[0]  # extract the coords of the one object
                 
