4886fda32edd9073221159a877efb4a68b5793d9
Jim Martens <github@2martens.de>
2019-07-07 13:43:19 +0200

    Added missing argument for function call
    Signed-off-by: Jim Martens <github@2martens.de>

--- src/twomartens/masterthesis/cli.py
+++ src/twomartens/masterthesis/cli.py
@@ -325,7 +325,7 @@ def _ssd_test(args: argparse.Namespace) -> None:
                                 predictor_sizes=ssd_model.predictor_sizes,
                                 batch_size=batch_size,
                                 resized_shape=image_size,
-                                training=False, evaluation=True)
+                                training=False, evaluation=True, augment=False)
     del file_names_photos, instances
 
     nr_digits = math.ceil(math.log10(math.ceil(length_dataset / batch_size)))
