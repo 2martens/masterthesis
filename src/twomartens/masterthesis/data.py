05d113a4ba00a9c7ce85d669e28127ee21c82b30
Jim Martens <github@2martens.de>
2019-08-07 13:20:34 +0200

    Convert image input to BGR
    Signed-off-by: Jim Martens <github@2martens.de>

--- src/twomartens/masterthesis/data.py
+++ src/twomartens/masterthesis/data.py
@@ -475,6 +475,7 @@ def load_scenenet_data(photo_paths: Sequence[Sequence[str]],
     else:
         transformations = [
             object_detection_2d_photometric_ops.ConvertTo3Channels(),
+            object_detection_2d_photometric_ops.ConvertColor(current='RGB', to='BGR'),
             object_detection_2d_geometric_ops.Resize(height=resized_shape[0],
                                                      width=resized_shape[1])
         ]
